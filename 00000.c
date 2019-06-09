3202b25a5b73e2866d0fb251b570071d9eae8afb
chullin <fg6ts15@gmail.com>
2019-06-10 01:50:24 +0800

    1080610

--- 00000.c
+++ 00000.c
@@ -5,7 +5,7 @@ int main(){
 	int b[5];
 	int c[5];
 	int count = 0;
-	int temp, max;
+	int temp, max;o
 	printf("Please enter 5 numbers: \n");
 	scanf("%d %d %d %d %d",&a[0], &a[1], &a[2], &a[3], &a[4]);
 	scanf("%d %d %d %d %d",&b[0], &b[1], &b[2], &b[3], &b[4]);
